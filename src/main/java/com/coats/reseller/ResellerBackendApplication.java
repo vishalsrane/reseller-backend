64b21043675c9b36ddd3df932a7b7543317b9b57
Vishal <vishal.rane@coats.com>
2020-06-10 06:52:24 +0530

    This is first commit.

---
+++ src/main/java/com/coats/reseller/ResellerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coats.reseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coats/reseller/ResellerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coats.reseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
